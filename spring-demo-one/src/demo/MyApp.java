f59aa5045647842f306842cb25e285369f8a4a51
john eipe <john.eipe@siriuscom.com>
2017-08-02 00:01:50 +0530

    Changed to Pure annotation based

--- spring-demo-one/src/demo/MyApp.java
+++ spring-demo-one/src/demo/MyApp.java
@@ -1,24 +1,34 @@
 package demo;
 
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+@Configuration
+@ComponentScan("demo")
 public class MyApp {
 	
 	public static void main(String[] args) {
 		
-		// load the spring configuration file
-		ConfigurableApplicationContext context = 
-				new ClassPathXmlApplicationContext("myConfig.xml");
-		
+		 /* Spring IoC Without XML */
+	    AnnotationConfigApplicationContext context
+	        = new AnnotationConfigApplicationContext(MyApp.class);
 		
 		Car c =  context.getBean("car", Car.class);
 		c.startCar();
 		
-				
 		// close the context
 		context.close();
 	}
+	
+	@Bean
+	public Car getCar(){
+		return new Car();
+	}
+	
 }
 
 
